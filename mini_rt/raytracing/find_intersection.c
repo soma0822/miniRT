fa13e5ee5a50593e5cf3230a64cd17caf49d0816
sinagaki <soma0822soma@gmail.com>
2023-09-12 17:07:47 +0900

    a

--- mini_rt/raytracing/find_intersection.c
+++ mini_rt/raytracing/find_intersection.c
@@ -6,7 +6,7 @@
 /*   By: sinagaki <sinagaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 18:21:14 by sinagaki          #+#    #+#             */
-/*   Updated: 2023/09/12 16:23:50 by sinagaki         ###   ########.fr       */
+/*   Updated: 2023/09/12 17:06:25 by sinagaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,18 @@
 
 t_shader_params *find_intersection(t_world *world, t_object *object, t_vector screen_vec)
 {
-	if (object->type == SPHERE)
-		return (find_intersection_sphere(world, object, screen_vec));
-	else if (object->type == PLANE)
-		return (find_intersection_plane(world, object, screen_vec));
-	else if (object->type == CYLINDER)
-		return (find_intersection_cylinder(world, object, screen_vec));
-	return (0);
+	t_object *tmp;
+	t_shader_params *material;
+
+	tmp = object;
+	while (tmp != NULL)
+	{
+		if (object->type == SPHERE)
+			material = find_intersection_sphere(world, object, screen_vec));
+		else if (object->type == PLANE)
+			return (find_intersection_plane(world, object, screen_vec));
+		else if (object->type == CYLINDER)
+			return (find_intersection_cylinder(world, object, screen_vec));
+		
+	}
 }
