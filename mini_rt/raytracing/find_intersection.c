f25d4c78aeac33824c9cc915469c32520c3aff5b
sinagaki <soma0822soma@gmail.com>
2023-09-12 17:16:18 +0900

    a

--- mini_rt/raytracing/find_intersection.c
+++ mini_rt/raytracing/find_intersection.c
@@ -6,7 +6,7 @@
 /*   By: sinagaki <sinagaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 18:21:14 by sinagaki          #+#    #+#             */
-/*   Updated: 2023/09/12 17:08:24 by sinagaki         ###   ########.fr       */
+/*   Updated: 2023/09/12 17:15:28 by sinagaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,21 @@ t_shader_params *find_intersection(t_world *world, t_object *object, t_vector sc
 {
 	t_object *tmp;
 	t_shader_params *material;
+	t_shader_params *ret;
 
 	tmp = object;
+	ret = NULL;
 	while (tmp != NULL)
 	{
 		if (object->type == SPHERE)
-			material = find_intersection_sphere(world, object, screen_vec));
+			material = find_intersection_sphere(world, object, screen_vec);
 		else if (object->type == PLANE)
-			return (find_intersection_plane(world, object, screen_vec));
+			material = find_intersection_plane(world, object, screen_vec);
 		else if (object->type == CYLINDER)
-			return (find_intersection_cylinder(world, object, screen_vec));
-		
+			material = find_intersection_cylinder(world, object, screen_vec);
+		if (ret == NULL || ret->distance > material->distance)
+			ret = material;
+		tmp = tmp->next;
 	}
+	return (ret);
 }
--- mini_rt/raytracing/shader_init.c
+++ mini_rt/raytracing/shader_init.c
@@ -3,23 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   shader_init.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khorike <khorike@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sinagaki <sinagaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 16:19:32 by sinagaki          #+#    #+#             */
-/*   Updated: 2023/09/12 16:28:47 by khorike          ###   ########.fr       */
+/*   Updated: 2023/09/12 17:13:54 by sinagaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
 #include "include.h"
 
-t_shader_params	*shader_init(t_vector position, t_object obj, t_world world)
+t_shader_params	*shader_init(t_vector position, t_object obj, t_world world, double distance)
 {
 	t_shader_params	*material;
 
 	material = ft_calloc(sizeof(t_shader_params), 1);
 	if (!material)
 		ft_error("Memory allocation error!\n");
+	material->distance = distance;
 	material->position = position;
 	material->normal = vector_normalize(vector_sub(position, *obj.pos));
 	material->light_dir = \
