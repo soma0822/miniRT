44dc9d794f5bad5618d6cfac66369a8450f65f07
khorike <horike.kaisyu@gmail.com>
2023-09-14 16:06:31 +0900

    fi main norm

--- main.c
+++ main.c
@@ -3,20 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sinagaki <sinagaki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: khorike <khorike@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2023/09/13 22:31:32 by sinagaki         ###   ########.fr       */
+/*   Created: 2023/09/14 15:23:33 by khorike           #+#    #+#             */
+/*   Updated: 2023/09/14 16:05:35 by khorike          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "mini_rt.h"
 #include "include.h"
 #include "material.h"
-#include "matrix.h"
 
-void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+static void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
 	int	*dst;
 
@@ -24,48 +22,65 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-int raytracing(t_world *world)
+static t_vector	calculate_ray_direction(t_world *world,
+			t_screen screen, double x, double y)
 {
+	double		sw;
+	double		sh;
+	t_vector	xx;
+	t_vector	yy;
+
+	sw = x - (world->screen_width - 1) / 2;
+	sh = (world->screen_height - 1) / 2 - y;
+	xx = vector_mult(screen.x, sw);
+	yy = vector_mult(screen.y, sh);
+	return (vector_normalize(vector_add(screen.center, vector_add(xx, yy))));
+}
+
+t_ray	calculate_ray(t_world *world, t_screen screen, double x, double y)
+{
+	t_vector	ray_direction;
+	t_ray		ray;
+
+	ray_direction = calculate_ray_direction(world, screen, x, y);
+	world->dir_vec = ray.direction;
+	ray.direction = ray_direction;
+	ray.start = *world->camera->pos;
+	return (ray);
+}
+
+static int	raytracing(t_world *world)
+{
+	t_screen		screen;
+	static double	y = 0;
+	double			x;
+	t_ray			ray;
+	t_shader_params	*params;
+
 	world->camera->aspect_ratio = world->screen_width / world->screen_height;
-	t_screen screen;
 	screen = screen_new(world->camera, world);
-    for (double y = 0; y < world->screen_height; y++)
-    {
-        for (double x = 0; x < world->screen_width; x++)
-        {
-            // スクリーン座標からワールド座標への変換
-			double sw = x - (world->screen_width - 1) / 2;
-			double sh = (world->screen_height - 1) / 2 - y;
-			t_vector xx = vector_mult(screen.x, sw);
-			t_vector yy = vector_mult(screen.y, sh);
-			t_vector ray_direction;
-			ray_direction = vector_normalize(vector_add(screen.center, vector_add(xx, yy)));
-            // screen_vec = vector_init((2 * x / world->screen_width - 1.0), 2 * y / world->screen_height - 1.0, 0);
-            // 方向ベクトル
-            world->dir_vec = ray_direction;
-            t_ray ray;
-            ray.direction = world->dir_vec;
-            ray.start = *world->camera->pos;
-            // 交点の検出
-            t_shader_params *params = find_intersection(world, world->objects, ray);
+	while (y < world->screen_height)
+	{
+		x = 0;
+		while (x < world->screen_width)
+		{
+			ray = calculate_ray(world, screen, x, y);
+			params = find_intersection(world, world->objects, ray);
 			if (params == NULL)
-            {
-                my_mlx_pixel_put(&world->img, x, y, rgb2hex(0,0,0));
-            }
-            else
-            {
-				t_color tmp;
-				tmp = calculate_light_effect(world, *params);
-                my_mlx_pixel_put(&world->img, x, y, color2hex(tmp));
-            }
-        }
-    }
-    return (0);
+				my_mlx_pixel_put(&world->img, x, y, rgb2hex(0, 0, 0));
+			else
+				my_mlx_pixel_put(&world->img, x, y,
+					color2hex(calculate_light_effect(world, *params)));
+			x++;
+		}
+		y++;
+	}
+	return (0);
 }
 
 int	main(int ac, char **av)
 {
-	t_world *world;
+	t_world	*world;
 
 	if (ac != 2)
 		ft_error("Argument error!\n");
@@ -75,17 +90,17 @@ int	main(int ac, char **av)
 	world->mlx_ptr = mlx_init();
 	world->screen_width = 800;
 	world->screen_height = 600;
-	world->win_ptr = mlx_new_window(world->mlx_ptr, world->screen_width, world->screen_height, "RayTracing with miniRT");
-	world->img.img_ptr = mlx_new_image(world->mlx_ptr, world->screen_width, world->screen_height);
-	world->img.data = (int *)mlx_get_data_addr(world->img.img_ptr, &world->img.bpp, &world->img.line_size, &world->img.endian);
-
+	world->win_ptr = mlx_new_window(world->mlx_ptr, world->screen_width,
+			world->screen_height, "RayTracing with miniRT");
+	world->img.img_ptr = mlx_new_image(world->mlx_ptr, world->screen_width,
+			world->screen_height);
+	world->img.data = (int *)mlx_get_data_addr(world->img.img_ptr,
+			&world->img.bpp, &world->img.line_size, &world->img.endian);
 	raytracing(world);
-	// ft_print_world(world);
-
-	mlx_put_image_to_window(world->mlx_ptr, world->win_ptr, world->img.img_ptr, 0, 0);
+	mlx_put_image_to_window(world->mlx_ptr, world->win_ptr,
+		world->img.img_ptr, 0, 0);
 	mlx_hook(world->win_ptr, 17, 0, close_window, &world);
 	mlx_hook(world->win_ptr, 2, 0, esc_hook, &world);
 	mlx_loop(world->mlx_ptr);
-
 	return (0);
 }
